faf8c0da54a547a6c17ad0459a3c0c72eca67d48
seyyedalidevotee <103094091+seyyedalidevotee@users.noreply.github.com>
2022-05-10 00:35:30 +0430

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
         System.out.println("\t\t\t\t\t*\t\t\t\t\t\t\t\t*");
         System.out.println("\t\t\t\t*\t\t*\t\t\t\t\t\t*\t\t*");
         System.out.println("\t\t\t*\t\t\t\t*\t\t\t\t*\t\t\t\t*");
