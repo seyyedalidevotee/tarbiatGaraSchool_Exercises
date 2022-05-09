4e4a848c4a6328d5d9d33e5639ab6771e2deb1da
seyyedalidevotee <103094091+seyyedalidevotee@users.noreply.github.com>
2022-05-10 00:32:32 +0430

    تمرین شماره یک
    حرف ام را در محیط کنسول به نمایش در آورید

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\t\t\t\t\t*\t\t\t\t\t\t\t\t*");
+        System.out.println("\t\t\t\t*\t\t*\t\t\t\t\t\t*\t\t*");
+        System.out.println("\t\t\t*\t\t\t\t*\t\t\t\t*\t\t\t\t*");
+        System.out.println("\t\t*\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t*");
+        System.out.println("\t*\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*");
+    }
+}
