193536a107d7d727655c7e551588b015a97a8e27
seyyedalidevotee <103094091+seyyedalidevotee@users.noreply.github.com>
2022-05-10 19:47:35 +0430

    چاپ ام با ارتفاعی که کاربر تعیین می کند
    چاپ ام با ارتفاعی که کاربر تعیین می کند

--- Main.java
+++ Main.java
@@ -5,10 +5,34 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("\t\t\t\t\t*\t\t\t\t\t\t\t\t*");
-        System.out.println("\t\t\t\t*\t\t*\t\t\t\t\t\t*\t\t*");
-        System.out.println("\t\t\t*\t\t\t\t*\t\t\t\t*\t\t\t\t*");
-        System.out.println("\t\t*\t\t\t\t\t\t*\t\t*\t\t\t\t\t\t*");
-        System.out.println("\t*\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*");
+        Scanner scanner = new Scanner(System.in);
+        int lines = scanner.nextInt();
+        bt(lines);
+
     }
+    //تابع اصلی چاپ m
+    public static void bt(int a) {
+        int b = a;
+        for (int i = a; i > 0; i--) {
+            btr(i);
+            btr((b - i) * 2);
+            btr((i - 1) * 2);
+            btr((b - i) * 2);
+            System.out.println("");
+
+        }
+    }
+    //تابع چاپ فاصله + *
+    public static void btr(int e) {
+        if (e != 0) {
+            for (int i = 0; i < e; i++) {
+                System.out.print("\t");
+            }
+            System.out.print("*");
+        } else {
+            System.out.print("*");
+        }
+    }
+
+
 }
